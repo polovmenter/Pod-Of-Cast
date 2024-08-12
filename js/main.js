62244f5d8094d9e4e2c1afe7e40311d2e96bf09e
Andrey Danilov <polovmenter840@gmail.com>
2024-08-12 17:13:00 +0300

    Сделал и адаптировал testimonials

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const testimonialsSlider = new Swiper(".slider__inner", {
+  navigation: {
+      nextEl: '.slider__button-next',
+      prevEl: '.slider__button-prev',
+  },
+
+  spaceBetween: 20,
+  slidesPerView: "auto",
+})
