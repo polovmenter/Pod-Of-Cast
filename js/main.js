a0257c703a3c83ca0f35bd501f8ec45122d3e699
Andrey Danilov <polovmenter840@gmail.com>
2024-08-17 01:26:31 +0300

    Сделал contact и mobile-menu, адаптировал contact, исправил высоту строки у button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const burgerButton = document.querySelector(".burger-button")
+const closeButton = document.querySelector(".mobile-menu__close-button")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+burgerButton.addEventListener("click", () => {
+  mobileMenu.classList.add("mobile-menu--open")
+})
+
+closeButton.addEventListener("click", () => {
+  mobileMenu.classList.remove("mobile-menu--open")
+})
+
 const testimonialsSlider = new Swiper(".slider__inner", {
   navigation: {
       nextEl: '.slider__button-next',
