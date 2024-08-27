769a61e2349f25d90b806f624ea15826f89031f1
Andrey Danilov <polovmenter840@gmail.com>
2024-08-28 01:12:55 +0300

    Сделал episodes, добавил страницы для других подкастов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const burgerButton = document.querySelector(".burger-button")
 const closeButton = document.querySelector(".mobile-menu__close-button")
 const mobileMenu = document.querySelector(".mobile-menu")
+const categories = document.querySelectorAll(".categories__button")
+const categoriesItems = document.querySelectorAll(".categories__item")
 
 burgerButton.addEventListener("click", () => {
   mobileMenu.classList.add("mobile-menu--open")
@@ -12,10 +14,28 @@ closeButton.addEventListener("click", () => {
 
 const testimonialsSlider = new Swiper(".slider__inner", {
   navigation: {
-      nextEl: '.slider__button-next',
-      prevEl: '.slider__button-prev',
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
   },
 
   spaceBetween: 20,
   slidesPerView: "auto",
+})
+
+document.querySelector(".categories__buttons").addEventListener("click", (event) => {
+  if (event.target.tagName !== "BUTTON") return false
+
+  let target = event.target
+  let filterTag = event.target.dataset.filter;
+
+  categories.forEach(item => item.classList.remove("categories__button--active"))
+  target.classList.add("categories__button--active")
+
+  categoriesItems.forEach(item => {
+    item.classList.remove("categories__item--hide")
+
+    if (!item.dataset.tag.includes(filterTag) && filterTag !== "all") {
+      item.classList.add("categories__item--hide")
+    }
+  })
 })
